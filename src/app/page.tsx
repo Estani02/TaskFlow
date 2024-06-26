fce5f20caeaecb17319172d1ce94906af6f72ee5
Cuenta Personal <estaniolmedo@gmail.com>
2024-06-26 14:01:53 -0300

    change h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex w-full flex-col items-center justify-center gap-10 py-36">
-      <h1 className="text-4xl font-bold uppercase">TaskFlow - technical test</h1>
+      <h1 className="text-center text-4xl font-bold uppercase">TaskFlow</h1>
       <div className="flex flex-col items-center justify-center gap-10">
         <Link passHref href="/tasks">
           <Button variant="contained">Tasks</Button>
